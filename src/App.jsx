6e6746fcfcd5de49a4589a9430672e1930aed00a
su139t <su139t@gmail.com>
2025-06-16 23:22:54 +0530

    changes in app file for testing

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <>
       <div className="h-200 w-full bg-gray-800 text-white">
-        <h1>Hyy Su139t...</h1>
+        <h1>Hello Su139t...</h1>
       </div>
     </>
   );
