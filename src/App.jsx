e6304064f9496af53c85426ff3ab42aa395c7054
su139t <su139t@gmail.com>
2025-06-16 23:14:37 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="h-200 w-full bg-gray-800 text-white">
+        <h1>Hyy Su139t...</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
